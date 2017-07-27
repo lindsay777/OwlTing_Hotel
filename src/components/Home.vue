f5c4fc270f7f0829aad92e1323bc587809684ad4
lindsay777 <lindsaytseng777@gmail.com>
2017-07-27 18:01:58 +0800

    home columns

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,40 +5,40 @@
 
       <div class="content">
         <!-- 最上排三個 -->
-        <div class="row">
-          
-          <div class="col-sm-4">
+        <div class="columns">
+
+          <div class="column">
             <div class="card room-top">
               <div class="card-block">
                 <h3 class="card-title">退房進度</h3>
                 <p class="card-text">尚有 2 間未退房</p>
-                <!-- <a href="#/checkout" class="btn btn-primary">辦理退房</a> -->
               </div>
             </div>
           </div>
-          <div class="col-sm-4">
+
+          <div class="column">
             <div class="card room-top">
               <div class="card-block">
                 <h3 class="card-title">預約入住</h3>
                 <p class="card-text">尚有 2 間未入住</p>
-                <!-- <a href="#/checkin" class="btn btn-primary">辦理入住</a> -->
               </div>
             </div>
           </div>
-          <div class="col-sm-4">
+
+          <div class="column">
             <div class="card room-top">
               <div class="card-block">
                 <h3 class="card-title">今日空房</h3>
                 <p class="card-text">尚有 12 間空房</p>
-                <!-- <a href="#/sell" class="btn btn-primary">現場銷售</a> -->
               </div>
             </div>
           </div>
         </div>
+
         <!-- 原本的四格 -->
-        <!-- <div class="row"> -->
+        <div class="columns">
 
-          <div class="col-md-6">
+          <div class="column">
             <div class="card text-center room">
               <div class="card-header">單人房</div>
               <div class="card-block">
@@ -46,7 +46,29 @@
                 <a href="#" class="button is-primary is-focused">查看房型</a>
               </div>
             </div>
+          </div>
 
+          <div class="column">
+            <div class="card text-center room">
+              <div class="card-header">雙人房</div>
+              <div class="card-block">
+                <h4 class="card-title">經典</h4>
+                <a href="#" class="button is-primary is-focused">查看房型</a>
+              </div>
+            </div>
+          </div>
+          
+          <div class="column">
+            <div class="card text-center room">
+              <div class="card-header">單人房</div>
+              <div class="card-block">
+                <h4 class="card-title">經典</h4>
+                <a href="#" class="button is-primary is-focused">查看房型</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="column">
             <div class="card text-center room">
               <div class="card-header">雙人房</div>
               <div class="card-block">
@@ -56,25 +78,7 @@
             </div>
           </div>
 
-          <div class="col-md-6">
-            <div class="card text-center room">
-              <div class="card-header">單人房</div>
-              <div class="card-block">
-                <h4 class="card-title">經典</h4>
-                <a href="#" class="button is-primary is-focused">查看房型</a>
-              </div>
-            </div>
-
-            <div class="card text-center room">
-              <div class="card-header">雙人房</div>
-              <div class="card-block">
-                <h4 class="card-title">經典</h4>
-                <a href="#" class="button is-primary is-focused">查看房型</a>
-              </div>
-            </div>
-
-          </div>
-
+        </div>
         <!-- </div> -->
 
       </div>
